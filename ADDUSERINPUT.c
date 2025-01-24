2da6a410be2d2b7ec9c48fb5df3d90a152013803
SukanyaChakraborty <sukanyachakraborty789@gmail.com>
2025-01-24 16:46:29 +0530

    Add files via upload

---
+++ ADDUSERINPUT.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("ENTER THE NUMBERS");
+	scanf ("%d",&a);
+	scanf ("%d",&b);
+	int s=a+b;
+	printf("The sum of %d and %d is %d", a,b,s);
+	return 0;
+	
+}
