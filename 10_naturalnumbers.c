d9491510dbf897e923e6e2ed2d9b6faedafaa664
SukanyaChakraborty <sukanyachakraborty789@gmail.com>
2025-01-25 18:37:09 +0530

    Add files via upload

---
+++ 10_naturalnumbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int i,sum=0;
+	for(i=1;i<=10; i++)
+	{
+		sum+= pow(i,2);
+	}
+	 printf("THE SUM OF THE SQUARES OF THE FIRST 10 NATURAL NUMBERS IS: %d\n",sum);
+	 return 0;
+	
+}
