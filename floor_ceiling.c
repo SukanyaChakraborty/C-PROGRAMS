df59629dea5bf61901fb344f43e958287eb5bb67
SukanyaChakraborty <sukanyachakraborty789@gmail.com>
2025-01-25 18:36:09 +0530

    Add files via upload

---
+++ floor_ceiling.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	float num1=3.7, num2=2.3, num3= 5.9;
+	printf("%.0f%.0f\n",floor(3.7), ceil(3.7));
+	printf("%.0f%.0f\n",floor(2.3), ceil(2.3));
+	printf("%.0f%.0f\n",floor(5.9), ceil(5.9));
+	return 0;
+}
