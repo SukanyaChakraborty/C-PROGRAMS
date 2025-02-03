c7bd1d1885435df7998c95dbab7a371d27b280f4
SukanyaChakraborty <sukanyachakraborty789@gmail.com>
2025-02-03 10:49:28 +0530

    Add files via upload

---
+++ positive_negative.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("ENTER A NUMBER :");
+	scanf("%d", &n);
+	if(n>=0)
+	printf("THE NUMBER %d IS POSITIVE",n);
+	else
+	printf("THE NUMBER %d IS NEGATIVE",n);
+	return 0;
+}
+	
