b5ccec7079d8a2b95f7f384f920c9765f15fdf84
SukanyaChakraborty <sukanyachakraborty789@gmail.com>
2025-02-23 12:28:11 +0530

    Add files via upload

---
+++ ODDNATURALNUMBERS.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int i,sum=0;
+	for(i=1;i<20; i=i+2)
+	{
+		sum+=i;
+	}
+	 printf("THE SUM OF THE OF THE FIRST 10 ODD NATURAL NUMBERS IS: %d\n",sum);
+	 return 0;
+	
+}
