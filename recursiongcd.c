aa4a27c866bb727d599c215c8866d1fc84ebf821
SukanyaChakraborty <sukanyachakraborty789@gmail.com>
2025-03-22 10:52:04 +0530

    Add files via upload

---
+++ recursiongcd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int gcd(int a,int b)
+{
+	if(b==0)
+	return a;
+    return gcd(b,a);
+}
+int main()
+{
+	int a,b;
+	printf("ENTER TWO NUMBERS: ");
+	scanf("%d %d, &a,&b");
+	printf("GCD of %d and %d is %d\n",a,b,gcd(a,b));
+	return 0;
+
+}
